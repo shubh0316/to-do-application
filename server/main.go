25fd291f6c376604a289c87e5eb79b59ee8da657
shubhanker <ankursaxena347@gmail.com>
2023-10-13 16:16:06 +0530

    ui begins

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type Todo struct {
@@ -18,6 +19,10 @@ func main() {
 	fmt.Print("Hello World")
 
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 	todos := []Todo{}
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
